b2bef83825bec308dcff7aa00c10cd8bf2ba6004
salvatoreromeo <info@salvatoreromeo.com>
2022-02-19 16:51:48 +0100

    fix relative path deploy

--- packages/redevtools.com/.eleventy.js
+++ packages/redevtools.com/.eleventy.js
@@ -6,6 +6,30 @@ const fastglob = require(`fast-glob`); // 11ty uses `fast-glob` internally
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 
+async function blogEntries(){
+    const entries = await fastglob([`src/blog/**/index.md`], { cwd: "." });
+    return entries
+}
+
+
+const manageRelativeImages = async (changedFiles) => {
+    for(const f of changedFiles){
+        let fileFullPath = path.join(__dirname, f);
+        const filePath = path.parse(fileFullPath)
+        const content = fs.readFileSync(fileFullPath).toString()
+        const matches = content.match(/permalink: (.*)/i)
+        if(matches && matches.length > 0){
+            const permalink = matches[1]
+            const entries = await fastglob([`*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg}`], { cwd: filePath.dir });
+            for(let e of entries){
+                const src = path.join(filePath.dir, e);
+                const dst = path.join("dist", permalink, e);
+                await fs.promises.copyFile(src, dst);
+                console.log(`✓ ${e} ➞ ${dst}`)
+            }
+        }
+    }
+}
 module.exports = function(eleventyConfig) {
     for(const dir of ["dist/assets/css"])
     if (!fs.existsSync(dir)){
@@ -17,26 +41,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/fonts");
     eleventyConfig.setWatchJavaScriptDependencies(false);
     eleventyConfig.addWatchTarget("./src/");
-    eleventyConfig.on('beforeWatch', async (changedFiles) => {
-        console.log("changedFiles: ",changedFiles)
-        for(const f of changedFiles){
-            let fileFullPath = path.join(__dirname, f);
-            const filePath = path.parse(fileFullPath)
-            const content = fs.readFileSync(fileFullPath).toString()
-            const matches = content.match(/permalink: (.*)/i)
-            if(matches && matches.length > 0){
-                const permalink = matches[1]
-                const entries = await fastglob([`*.{jpg,jpeg,png,gif,webp,mp3,mp4,webm,ogg}`], { cwd: filePath.dir });
-                for(let e of entries){
-                    const src = path.join(filePath.dir, e);
-                    const dst = path.join("dist", permalink, e);
-                    await fs.promises.copyFile(src, dst);
-                    console.log(`✓ ${e} ➞ ${dst}`)
-                }
-            }
-        }
+    eleventyConfig.on('beforeWatch', manageRelativeImages)
+    eleventyConfig.on('afterBuild', async ()=>{
+        const foundBlogEntries = await blogEntries()
+        await manageRelativeImages(foundBlogEntries)
     })
     eleventyConfig.on("beforeBuild", () => {
+        
         let result = sass.renderSync({
             file: "src/assets/css/styles.scss",
             sourceMap: false,
